1b6e47f9802bdd208448e8bd4f5029d340a9d0fb
Iuliia El Sammak <juliag875@gmail.com>
2022-01-13 11:23:37 -0500

    commit tree 1bd5e91be0ae9237fccfe096a68c302b0aa6aff4

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+ const superWin = record.find((record) => record.result === "W");
+    if (superWin === undefined) {
+        return undefined;
+    } else {
+        return superWin.year;
+    }
+}
